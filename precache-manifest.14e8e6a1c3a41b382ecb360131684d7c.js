53f063588f01b4f81ecae13613478daac1518b64
Supriyo Santra <supriyos@capitalnumbers.com>
2022-07-04 15:16:45 +0530

    Updates

---
+++ precache-manifest.14e8e6a1c3a41b382ecb360131684d7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d957d0f3b13a4eb1ef49",
+    "url": "/react_filter/static/css/main.0f9d6dd6.chunk.css"
+  },
+  {
+    "revision": "d957d0f3b13a4eb1ef49",
+    "url": "/react_filter/static/js/main.d957d0f3.chunk.js"
+  },
+  {
+    "revision": "12a547d5be2359b88d04",
+    "url": "/react_filter/static/js/1.12a547d5.chunk.js"
+  },
+  {
+    "revision": "63bd2221984275853b0d",
+    "url": "/react_filter/static/js/runtime~main.63bd2221.js"
+  },
+  {
+    "revision": "2b13193d1624915beb75facf39c84bcf",
+    "url": "/react_filter/index.html"
+  }
+];
